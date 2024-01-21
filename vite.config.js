008522b35da55ea9a53d5b459907de94b9c8320d
herrmik <michalher@seznam.cz>
2024-01-21 19:34:07 +0100

    2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://herrmik.github.io/bank_modern_app/',
   plugins: [react()],
 });
